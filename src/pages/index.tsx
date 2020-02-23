eebb40f0d1fc30f63c4e23c1d38bad3eb7e4ba67
James Cockbain <james.cockbain@ibm.com>
2020-02-23 16:19:23 +0000

    style for mobiles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,17 +18,7 @@ const HomePage = (props: Props) => {
       >
         <h1>Hi, I'm James</h1>
         <p>I'm a full stack software developer, working for IBM.</p>
-        <img
-          src={ProfilePic}
-          alt="a nice picture of me"
-          style={{
-            display: "block",
-            borderRadius: `10px`,
-            margin: `auto`,
-            width: `50%`,
-            marginBottom: `2em`,
-          }}
-        />
+        <img src={ProfilePic} alt="a nice picture of me" />
         <p>
           You can read more about me, and why I started this site,{" "}
           <FadeLink to="/pages/me">here</FadeLink>.
