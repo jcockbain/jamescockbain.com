55e95656a22f3f2deaa3bd337330fb1d34d3d0ef
James Cockbain <james.cockbain@ibm.com>
2020-06-02 21:56:24 +0100

    use card on home page
    Signed-off-by: James Cockbain <james.cockbain@ibm.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,18 +10,20 @@ const HomePage = (props: Props) => {
   return (
     <Layout location={props.location} title="Home">
       <SEO title="home" />
-      <div className="container">
-        <h3>Hi, I'm James &#x1f44b; </h3>
-        <p>I'm a software developer, working for IBM.</p>
-        <img
-          src={ProfilePic}
-          className="profile-picture"
-          alt="A picture of me!"
-        />
-        <p>
-          You can read more about me, and why I started this site,{" "}
-          <FadeLink to="/pages/about-me">here</FadeLink>.
-        </p>
+      <div className="card">
+        <div className="card-body">
+          <h2>Hi, I'm James &#x1f44b; </h2>
+          <p>I'm a software developer, working for IBM.</p>
+          <img
+            src={ProfilePic}
+            className="profile-picture"
+            alt="A picture of me!"
+          />
+          <p>
+            You can read more about me,{" "}
+            <FadeLink to="/pages/about-me">here</FadeLink>.
+          </p>
+        </div>
       </div>
     </Layout>
   )
